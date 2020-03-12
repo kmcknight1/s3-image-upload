fd839e7645d90c092693a13951fa7603a0d68c56
kmcknight1 <katiemcknight1991@gmail.com>
2020-03-12 12:51:39 -0500

    creates routes and makes nav with links in dashboard

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -4,12 +4,16 @@ import "./App.css";
 
 import Homepage from "./components/Homepage";
 import Login from "./components/Login";
+import SearchUsers from "./components/SearchUsers";
+import FriendRequests from "./components/FriendRequests";
 
 function App() {
   return (
     <div className="App">
       <Route exact path="/" component={Homepage} />
       <Route path="/login" component={Login} />
+      <Route path="/search-users" component={SearchUsers} />
+      <Route path="/friend-requests" component={FriendRequests} />
     </div>
   );
 }
--- react-client/src/components/Dashboard.js
+++ react-client/src/components/Dashboard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import FriendRequests from "./FriendRequests";
 import SearchUsers from "./SearchUsers";
@@ -7,8 +8,10 @@ export default function Dashboard() {
   return (
     <>
       <div>Dashboard</div>
-      <SearchUsers />
-      <FriendRequests />
+      <nav>
+        <Link to="/search-users">Find Friends</Link>
+        <Link to="/friend-requests">Friend Requests</Link>
+      </nav>
     </>
   );
 }
