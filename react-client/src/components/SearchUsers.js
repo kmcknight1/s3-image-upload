d0d42f5347fa1fb25187ca4d285cf86776466481
kmcknight1 <katiemcknight1991@gmail.com>
2020-03-12 12:56:46 -0500

    adds conditional to prevent showAll and filteredUsers from both appearing

--- react-client/src/components/SearchUsers.js
+++ react-client/src/components/SearchUsers.js
@@ -72,6 +72,7 @@ export default function SearchUsers() {
         {showAll ? "Hide All" : "Show All"}
       </button>
       {showAll &&
+        !filteredUsers &&
         allUsers.map(user => {
           return (
             <p
