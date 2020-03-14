001905e8b5e1514e24115e8838f2794ec6af09dd
kmcknight1 <katiemcknight1991@gmail.com>
2020-03-13 23:09:26 -0500

    adds call to api to get all user contacts if user is logged in

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
-import { Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Route, useHistory } from "react-router-dom";
 import "./App.css";
+import axios from "axios";
 
 import Homepage from "./components/Homepage";
 import Login from "./components/Login";
@@ -8,11 +9,40 @@ import SearchUsers from "./components/SearchUsers";
 import FriendRequests from "./components/FriendRequests";
 
 function App() {
+  const history = useHistory();
+  const [contacts, setContacts] = useState();
+  const id = localStorage.getItem("photostore_id");
+
+  useEffect(() => {
+    if (id) {
+      getContacts();
+    }
+  }, [id]);
+
+  const getContacts = () => {
+    axios
+      .get(`http://localhost:8888/api/contacts/${id}`)
+      .then(res => {
+        console.log(res.data);
+        setContacts([...res.data.sender, ...res.data.receiver]);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  const beep = "beep";
+
   return (
     <div className="App">
       <Route exact path="/" component={Homepage} />
       <Route path="/login" component={Login} />
-      <Route path="/search-users" component={SearchUsers} />
+      <Route
+        path="/search-users"
+        render={props => (
+          <SearchUsers {...props} contacts={contacts} beep={beep} />
+        )}
+      />
       <Route path="/friend-requests" component={FriendRequests} />
     </div>
   );
